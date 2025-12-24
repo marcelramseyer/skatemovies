d6e369677572ee094b03a7afea19fb020011a1a2
Marcel Ramseyer <marcel.ramseyer@hispeed.ch>
2025-12-24 13:55:18 +0100

    style: styling header

--- app/app.vue
+++ app/app.vue
@@ -4,13 +4,15 @@ import movies from '~/assets/data/skatemovies.json'
 
 <template>
   <div>
-    <header class="mx-auto max-w-2xl text-center my-16">
-      <h1 class="text-8xl text-gray-800 font-bold m-auto">SkateMovies</h1>
-      <div class="text-2xl italic text-gray-500 mt-2">Curated List of full-length skate movie classics, free to watch!
+    <header class="mx-auto max-w-2xl text-center mt-16 mb-20 ">
+      <h1 class="text-9xl text-gray-800 m-auto">SkateMovies
+      </h1>
+      <div class="text-2xl text-gray-500 mt-2">A curated List of Skate Film Classics,
+        full-lenth and free to watch!
       </div>
     </header>
     <main>
-      <ul class="text-lg flex gap-6 max-w-6xl m-auto">
+      <ul class="text-base flex gap-6 max-w-6xl m-auto">
         <li v-for="m in movies" :key="m.id">
           <!-- <img :src="`/images/${m.coverUrl}`" :alt="m.title" /> -->
           <NuxtImg :src="`/images/${m.coverUrl}`" class="rounded" :alt="m.title" width="400" format="webp"
@@ -22,3 +24,14 @@ import movies from '~/assets/data/skatemovies.json'
     </main>
   </div>
 </template>
+
+<style>
+body {
+  font-family: Instrument Sans, sans-serif;
+  background-color: #f9fafb;
+}
+
+h1 {
+  font-family: Instrument Serif, sans-serif;
+}
+</style>
