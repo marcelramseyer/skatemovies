b89e6a794d2495fd15546cd1a0cf6022b8adf604
Marcel Ramseyer <marcel.ramseyer@hispeed.ch>
2026-01-23 14:59:32 +0100

    changed config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/fonts', '@nuxt/image', '@nuxtjs/tailwindcss'],
-  ssr: false,
-  nitro: {
-    preset: 'cloudflare-pages'
-  }
+  ssr: false
 })
