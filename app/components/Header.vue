f6b2298c099b1d492401faf6c44ee2d7812f6b37
Marcel Ramseyer <marcel.ramseyer@hispeed.ch>
2026-01-24 10:22:37 +0100

    fix:favicon added

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="md:text-7xl text-5xl p-6 py-8 border-stone-800 my-8 mx-auto max-w-3xl">
-        <p class="text-center text-7xl mb-3 text-gray-50">❈</p>
+        <p class="text-center text-8xl mb-3 text-gray-50">※</p>
         <h1 class="text-gray-50 text-center">Skate Video Classics</h1>
         <div class="text-gray-400 md:text-xl text-lg text-center mt-4">A handpicked collection of legendary skate
             videos,
