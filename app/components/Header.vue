8f6366af222ffeed67dfda3d87639cb44a7458bf
Marcel Ramseyer <marcel.ramseyer@hispeed.ch>
2026-02-12 08:17:52 +0100

    content: changed hero title

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
     <header class="md:text-7xl text-5xl p-6 py-8 border-stone-800 my-16 mx-auto max-w-3xl">
         <h1 class="text-zinc-50 text-center">Skate Video Classics</h1>
-        <div class="text-zinc-400 md:text-xl text-lg text-center mt-6">A compilation of iconic skate
+        <div class="text-zinc-400 md:text-xl text-lg text-center mt-6">A collection of iconic skate
             videos from past decades.
             <br>
-            Watch them online for free and in full length!
+            Watch them on YouTube.
         </div>
     </header>
 </template>
